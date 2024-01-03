8d58cfd4ef19e4b2118d2d11e8dc358c67ee2513
Nishan <nishantkhatiwada012@gmail.com>
2024-01-03 10:35:21 +0545

    template edit and views logic

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from .views import *
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('category/<slug>', CategoryView.as_view(), name='category'),
+    path('productdetail/<slug>', ProductDetailView.as_view(), name='details'),
+    path('search', SearchView.as_view(), name='search'),
+    path('signup', signup, name='signup'),
+]
